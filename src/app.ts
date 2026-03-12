8a2dedca4d4fe62d6c4d1855c90d59b53767e088
Adittyaaa-codes <aditya.mallick.1512@gmail.com>
2026-03-12 06:02:58 +0530

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://studybot99.onrender.com'],
     credentials: true
 }));
 app.use(express.urlencoded({ extended: true }));
